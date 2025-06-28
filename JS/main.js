d861852b0b515cb21568a4123536e8633d1e7567
Gehiner <gehinerferley@gmail.com>
2025-06-28 08:00:05 -0500

    Carrusel Agregado

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,4 @@
-//Menú Hamburguesa 
+//----------------Menú Hamburguesa 
 const openMenu = document.querySelector(".menu__open");
 const closeMenu = document.querySelector(".menu__close");
 const nav = document.querySelector(".nav");
@@ -66,3 +66,55 @@ themeToggle.addEventListener("click", () => {
     localStorage.setItem("theme", "light");
   }
 });
+
+
+//--------------------Carrrusel
+
+const track = document.querySelector('.carousel-track');
+const items = document.querySelectorAll('.carousel-item');
+const nextBtn = document.querySelector('.carousel-btn.next');
+const prevBtn = document.querySelector('.carousel-btn.prev');
+const dotsContainer = document.querySelector('.carousel-dots');
+
+let currentIndex = 0;
+const totalItems = items.length;
+
+
+items.forEach((_, i) => {
+  const dot = document.createElement('span');
+  if (i === 0) dot.classList.add('active');
+  dotsContainer.appendChild(dot);
+});
+
+const dots = document.querySelectorAll('.carousel-dots span');
+
+function updateCarousel() {
+  track.style.transform = `translateX(-${currentIndex * 100}%)`;
+
+
+  dots.forEach(dot => dot.classList.remove('active'));
+  dots[currentIndex].classList.add('active');
+}
+
+nextBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % totalItems;
+  updateCarousel();
+});
+
+prevBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + totalItems) % totalItems;
+  updateCarousel();
+});
+
+dots.forEach((dot, i) => {
+  dot.addEventListener('click', () => {
+    currentIndex = i;
+    updateCarousel();
+  });
+});
+
+
+setInterval(() => {
+  currentIndex = (currentIndex + 1) % totalItems;
+  updateCarousel();
+}, 10000);
